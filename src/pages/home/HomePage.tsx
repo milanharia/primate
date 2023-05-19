4f71f0446ddf4061c6fe029694f0126c19872cd7
milan.haria <milanharia408@gmail.com>
2023-05-19 22:18:30 +0100

    feat(home): create basic page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,20 +1,34 @@
 import {
   IonContent,
   IonHeader,
+  IonImg,
+  IonMenuButton,
   IonPage,
-  IonTitle,
+  IonSearchbar,
   IonToolbar,
 } from "@ionic/react";
+import { searchSharp } from "ionicons/icons";
+import map from "./assets/map.png";
 
 export const HomePage: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
+        <IonToolbar color="transparent">
+          <div className="flex items-center px-4 bg-white border-black border p-2 mt-4 rounded-l-full rounded-r-full">
+            <IonSearchbar
+              searchIcon={searchSharp}
+              placeholder="Explore primate sites"
+              color="light"
+              style={{ padding: 0 }}
+            ></IonSearchbar>
+            <IonMenuButton color="dark"></IonMenuButton>
+          </div>
         </IonToolbar>
       </IonHeader>
-      <IonContent></IonContent>
+      <IonContent fullscreen>
+        <IonImg src={map} className="absolute inset-0 object-cover" />
+      </IonContent>
     </IonPage>
   );
 };
