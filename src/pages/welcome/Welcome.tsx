f2380a5aea7a4e3462fdd448fcb89fa930167628
milan.haria <milanharia408@gmail.com>
2023-05-19 21:13:10 +0100

    feat(welcome): add carousel skeleton

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,26 +1,46 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
+import { IonContent, IonHeader, IonIcon, IonImg, IonPage } from "@ionic/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { WelcomeScreen } from "./components/WelcomeScreen";
+
+import monkey from "./assets/monkey.svg";
+import background from "./assets/background.png";
+
+import "swiper/css";
+
+const SeeIllustratedGuidelinesButton = () => (
+  <button className="bg-black text-white font-bold py-2 px-6 rounded-l-full rounded-r-full mt-8 flex gap-2 items-center">
+    <span>See illustrated guidelines</span>
+    <IonIcon size="large" icon={monkey} />
+  </button>
+);
 
 export const WelcomePage: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Welcome</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Welcome</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonHeader></IonHeader>
+      <IonContent fullscreen className="flex flex-col">
+        <IonImg src={background} className="absolute inset-0 object-cover z0" />
+        <Swiper
+          className="h-full flex-1"
+          slidesPerView={1}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide className="h-full">
+            <WelcomeScreen title="Welcome to Primate" />
+          </SwiperSlide>
+          <SwiperSlide className="h-full">
+            <WelcomeScreen title=" Promoting ethical primate tourism" />
+          </SwiperSlide>
+          <SwiperSlide className="h-full">
+            <WelcomeScreen title="Helping you and primates stay safe">
+              <SeeIllustratedGuidelinesButton />
+            </WelcomeScreen>
+          </SwiperSlide>
+          <SwiperSlide className="h-full">
+            <WelcomeScreen title="Let’s get exploring!" />
+          </SwiperSlide>
+        </Swiper>
       </IonContent>
     </IonPage>
   );
